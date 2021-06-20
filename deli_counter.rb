e9ac941f170b98160b8bda92c68d67eaebcf4afa
amy-chinlai <amy@actionnetwork.org>
2021-06-20 11:28:56 -0400

    commit tree 7c28a4ccbc02d8600631edefafb7b217df9c6575

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+    if katz_deli == []
+        puts "The line is currently empty."
+    else 
+        katz_deli << "The line is currently: "
+        katz_deli.each_with_index do |name, index|
+        index = index.to_i + 1
+            puts "#{index}. #{name}"
+        end
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli.each_with_index do |name, index|
+     puts "Welcome, #{name}. You are number #{index + 1} in line."
+    end
+end
